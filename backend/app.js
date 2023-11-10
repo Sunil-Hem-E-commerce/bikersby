78d8d5c34f32d8501c75050705fde63eddff6b6b
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-11-10 22:34:22 +0545

    refactor: converted old code into async await and try-catch

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,20 @@
 const client = require("./connection.js");
 const express = require("express");
 const middleware = require("./utils/middleware.js");
+const logger = require("./utils/logger.js");
 
 const userRouter = require("./routers/users.js");
 
 const app = express();
+
+client
+  .connect()
+  .then(() => {
+    logger.info("Connected to DataBase!!");
+  })
+  .catch((error) => {
+    logger.error("Error while connecting to DataBase", error);
+  });
 
 app.listen(3001, () => {
   console.log("Sever is now listening at port 3001");
@@ -14,5 +24,3 @@ app.use("/api/users", userRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
-
-client.connect();
--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,15 +1,77 @@
 const userRouter = require("express").Router();
 const client = require("../connection.js");
 
-userRouter.get("/", (req, res) => {
-  client.query(`Select * from users`, (err, resDB) => {
-    if (!err) {
-      res.send(resDB.rows);
-    } else {
-      console.log("error", err.message);
-    }
-    client.end;
-  });
+userRouter.get("/", async (req, res) => {
+  try {
+    const resDB = await client.query(`Select * from users`);
+    res.send(resDB.rows);
+  } catch (error) {
+    next(error);
+  } finally {
+    await client.end();
+  }
 });
+
+userRouter.get("/:id", (req, res) => {
+  try {
+    client.query(
+      `Select * from users where id=${req.params.id}`,
+      (err, resDB) => {
+        res.send(resDB.rows);
+      }
+    );
+    client.end;
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+userRouter.post("/users", (req, res) => {
+  const user = req.body;
+  let insertQuery = `insert into users(user_id, full_name, assigned_task) 
+                         values(${user.id}, '${user.fName}', '${user.task}')`;
+
+  client.query(insertQuery, (err, resDB) => {
+    if (!err) {
+      res.send("Insertion was successful");
+    } else {
+      console.log(`ERROR:: ${err.message}`);
+    }
+  });
+  client.end;
+});
+
+userRouter.put("/users/:id", (req, res) => {
+  let user = req.body;
+  let id = req.params.id;
+  let updateQuery = `update users
+                         set full_name = '${user.fName}',
+                         assigned_task = '${user.task}'
+                         where user_id = ${id}`;
+
+  client.query(updateQuery, (err, resDB) => {
+    if (!err) {
+      res.send("Update was successful");
+    } else {
+      console.log(err.message);
+    }
+  });
+  client.end;
+});
+
+userRouter.delete("/users/:id", (req, res) => {
+  let deleteQuery = `delete from users where user_id=${req.params.id}`;
+
+  client.query(deleteQuery, (err, result) => {
+    if (!err) {
+      res.send("Deletion was successful");
+    } else {
+      console.log(err.message);
+    }
+  });
+  client.end;
+});
+
+// client.connect();
 
 module.exports = userRouter;
