ce6574ccadfc963da1c18a527e35a73f42e8fda2
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-11-26 18:41:17 +0545

    feat: stable node.js and express.js app

---
+++ Final_backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+// For post request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello there!!!");
+});
+
+module.exports = app;
---
+++ Final_backend/utils/config.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const SERVER_PORT = process.env.SERVER_PORT;
+
+module.exports = {
+  SERVER_PORT,
+};
