312d1ae0f5188fb64845729dbed1c57190afb2a2
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-11-27 00:03:00 +0545

    feat: added seeder file for roles table

---
+++ Final_backend/seeders/20231126180845-add-roles-in-roles-table.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "roles",
+      [
+        { roleName: "user" },
+        { roleName: "showroom" },
+        { roleName: "recondition" },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
