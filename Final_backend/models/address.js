5dbe8d56c1e6837f4bd8fca5a6f3965ae7c2776c
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-11-26 22:05:51 +0545

    migrate: create-address migration created

--- Final_backend/models/address.js
+++ Final_backend/models/address.js
@@ -1,24 +1,24 @@
 const address = sequelize.define(
   "address",
   {
-    address_id: {
+    addressId: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
       allowNull: false,
     },
-    address_line1: {
+    addressLine1: {
       type: DataTypes.STRING(255),
       allowNull: false,
     },
-    address_line2: {
+    addressLine2: {
       type: DataTypes.STRING(255),
     },
     city: {
       type: DataTypes.STRING(100),
       allowNull: false,
     },
-    district_id: {
+    districtId: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
