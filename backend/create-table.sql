63b1511d2d00a811ab807d5c34b4efcda245fe23
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-11-26 18:33:22 +0545

    refactor: refactor the table creation method according to new data model

--- backend/create-table.sql
+++ backend/create-table.sql
@@ -19,7 +19,14 @@ CREATE TABLE addresses (
     CONSTRAINT fk_address_district FOREIGN KEY (district_id) REFERENCES districts(district_id)
 );
 
+
 --03--
+CREATE TABLE roles (
+    role_id SERIAL PRIMARY KEY,
+    role_name VARCHAR(30) NOT NULL
+)
+
+--04--
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
@@ -31,20 +38,13 @@ CREATE TABLE users (
     default_address INT NOT NULL,
     user_status char NOT NULL,
     joined_on TIMESTAMP NOT NULL,
+    user_role INT NOT NULL,
     -- Need to configure disk to store logo as image and access via it's location.
-    profile_img VARCHAR(255),
+    profile_img TEXT,
     CONSTRAINT fk_user_address FOREIGN KEY (default_address) REFERENCES addresses(address_id)
+    CONSTRAINT fk_user_role FOREIGN KEY (user_role) REFERENCES roles(role_id)
 );
 
---04--
-CREATE TABLE showrooms (
-    showroom_id SERIAL PRIMARY KEY,
-    showroom VARCHAR(100) NOT NULL,
-    sr_phone VARCHAR(25) NOT NULL,
-    sr_email VARCHAR(255),
-    address_id INT NOT NULL,
-    CONSTRAINT fk_showroom_address FOREIGN KEY (address_id) REFERENCES addresses(address_id)
-);
 
 --05--
 CREATE TABLE products (
@@ -67,7 +67,7 @@ CREATE TABLE companies (
     company_id SERIAL PRIMARY KEY,
     company VARCHAR(100) NOT NULL,
     -- Need to configure disk to store logo as image and access via it's location.
-    logo VARCHAR(255)
+    logo TEXT
 );
 
 --08--
@@ -83,8 +83,7 @@ CREATE TABLE product_items (
     color_id INT NOT NULL,
     company_id INT NOT NULL,
     cat_id INT NOT NULL,
-    featured BOOLEAN NOT NULL DEFAULT FALSE,
-    price int NOT NULL,
+    isfeatured BOOLEAN NOT NULL DEFAULT FALSE,
     CONSTRAINT fk_product_item_product FOREIGN KEY (product_id) REFERENCES products(product_id),
     CONSTRAINT fk_product_item_colors FOREIGN KEY (color_id) REFERENCES colors(color_id),
     CONSTRAINT fk_product_item_company FOREIGN KEY (company_id) REFERENCES companies(company_id),
@@ -93,52 +92,54 @@ CREATE TABLE product_items (
 
 --10--
 CREATE TABLE ratings (
+    rating_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     product_id INT NOT NULL,
     indv_rating INT,
-    comment TEXT,
+    indv_commnet TEXT,
+    created_on TIMESTAMP NOT NULL,
     CONSTRAINT fk_rating_user FOREIGN KEY (user_id) REFERENCES users(user_id),
     CONSTRAINT fk_rating_product FOREIGN KEY (product_id) REFERENCES products(product_id),
-    CONSTRAINT pk_rating PRIMARY KEY (user_id, product_id)
+    UNIQUE (user_id, product_id)
 );
 
 --11--
 CREATE TABLE inventories (
-    inventory_id SERIAL NOT NULL UNIQUE,
-    showroom_id INT NOT NULL,
+    inventory_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
     item_id INT NOT NULL,
-    shipping CHAR NOT NULL DEFAULT 'y',
+    isShipping CHAR NOT NULL DEFAULT 'y',
     quantity INT NOT NULL DEFAULT 0,
-    sku VARCHAR(255),
-    CONSTRAINT fk_inventory_showroom FOREIGN KEY (showroom_id) REFERENCES showrooms(showroom_id),
+    price INT NOT NULL,
+    discounted_price INT NOT NULL,
+    CONSTRAINT fk_inventory_user FOREIGN KEY (user_id) REFERENCES users(user_id),
     CONSTRAINT fk_inventory_product_item FOREIGN KEY (item_id) REFERENCES product_items(item_id),
-    CONSTRAINT pk_inventories PRIMARY KEY (showroom_id, item_id)
+    UNIQUE (showroom_id, item_id)
 );
 
 --12--
-CREATE TABLE carts (
-    cart_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    inventory_id INT NOT NULL,
-    cart_quantity INT NOT NULL DEFAULT 1,
-    CONSTRAINT fk_cart_user FOREIGN KEY (user_id) REFERENCES users(user_id),
-    CONSTRAINT fk_cart_inventory FOREIGN KEY (inventory_id) REFERENCES inventories(inventory_id)
+CREATE TABLE Order_status_opts (
+    option_id SERIAL PRIMARY KEY,
+    order_status_value VARCHAR(50) NOT NULL,
 );
 
 --13--
 CREATE TABLE orders (
     order_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    order_status INT NOT NULL,
+    sub_total INT NOT NULL,
     order_placed TIMESTAMP NOT NULL,
-    order_status VARCHAR(255) NOT NULL DEFAULT 'Ready to ship!',
-    -- need to calculate before inserting price
-    order_price INT NOT NULL
+    CONSTRAINTS fk_orders_user FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
 
 --14--
 CREATE TABLE order_item (
     order_item_id SERIAL PRIMARY KEY,
     order_id INT NOT NULL,
-    cart_id INT NOT NULL,
+    qty INT NOT NULL DEFAULT 1,
+    price INT NOT NULL,
+    Inventory_id INT NOT NULL,
     CONSTRAINT fk_order_item_order FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    CONSTRAINT fk_order_item_cart FOREIGN KEY (cart_id) REFERENCES carts(cart_id)
+    CONSTRAINT fk_order_item_inventory FOREIGN KEY (inventory_id) REFERENCES inventories(inventory_id)
 );
