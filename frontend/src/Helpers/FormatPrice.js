709ceb16ebb23029813d91f9a3c8b77571db5724
Hemlal Dulal <hemlaldulal.986@gmail.com>
2023-11-27 14:10:35 +0545

    Feat: Added frontend

---
+++ frontend/src/Helpers/FormatPrice.js
@@ -0,0 +1,9 @@
+const FormatPrice = ({ price }) => {
+  return Intl.NumberFormat("en-NP", {
+    style: "currency",
+    currency: "NPR",
+    minimumFractionDigits: 2,
+  }).format(price / 100);
+};
+
+export default FormatPrice;
---
+++ frontend/src/reducer/filterReducer.js
@@ -0,0 +1,156 @@
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case "LOAD_FILTER_PRODUCTS": {
+      let priceArr = action.payload.map((curElem) => curElem.price);
+
+      //! Traditional way
+      // console.log(Math.max.apply(null, priceArr));
+
+      //! Using Reduce method
+      // let maxPrice = priceArr.reduce(
+      //   (initialVal, curElem) => Math.max(initialVal, curElem),
+      //   0
+      // );
+      // console.log(maxPrice);
+
+      //! Third method
+      let maxPrice = Math.max(...priceArr);
+      return {
+        ...state,
+        filter_products: [...action.payload],
+        all_products: [...action.payload],
+        filters: { ...state.filters, maxPrice, price: maxPrice },
+      };
+    }
+
+    case "SET_GRID_VIEW":
+      return {
+        ...state,
+        grid_view: true,
+      };
+
+    case "SET_LIST_VIEW":
+      return {
+        ...state,
+        grid_view: false,
+      };
+
+    case "GET_SORT_VALUE": {
+      // let userSortValue = document.getElementById("sort");
+      // let sort_value = userSortValue.options[userSortValue.selectedIndex].value;
+      return {
+        ...state,
+        sorting_value: action.payload,
+      };
+    }
+
+    case "SORTING_PRODUCTS": {
+      let newSortData;
+      // let tempSortProduct = [...action.payload];
+
+      const { filter_products, sorting_value } = state;
+      let tempSortProduct = [...filter_products];
+
+      const sortingProducts = (a, b) => {
+        if (sorting_value === "lowest") {
+          return a.price - b.price;
+        }
+
+        if (sorting_value === "highest") {
+          return b.price - a.price;
+        }
+
+        if (sorting_value === "a-z") {
+          return a.name.localeCompare(b.name);
+        }
+
+        if (sorting_value === "z-a") {
+          return b.name.localeCompare(a.name);
+        }
+      };
+
+      newSortData = tempSortProduct.sort(sortingProducts);
+
+      return {
+        ...state,
+        filter_products: newSortData,
+      };
+    }
+
+    case "UPDATE_FILTERS_VALUE": {
+      const { name, value } = action.payload;
+
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          [name]: value,
+        },
+      };
+    }
+    case "FILTER_PRODUCTS": {
+      let { all_products } = state;
+      let tempFiltereProduct = [...all_products];
+
+      const { text, category, company, color, price } = state.filters;
+
+      if (text) {
+        tempFiltereProduct = tempFiltereProduct.filter((curElem) => {
+          return curElem.name.toLowerCase().includes(text);
+        });
+      }
+
+      if (category !== "all") {
+        tempFiltereProduct = tempFiltereProduct.filter((curElem) => {
+          return curElem.category === category;
+        });
+      }
+
+      if (company !== "all") {
+        tempFiltereProduct = tempFiltereProduct.filter((curElem) => {
+          return curElem.company.toLowerCase() === company.toLowerCase();
+        });
+      }
+
+      if (color !== "all") {
+        tempFiltereProduct = tempFiltereProduct.filter((curElem) => {
+          return curElem.colors.includes(color);
+        });
+      }
+
+      if (price === 0) {
+        tempFiltereProduct = tempFiltereProduct.filter(
+          (curElem) => curElem.price === price
+        );
+      } else {
+        tempFiltereProduct = tempFiltereProduct.filter(
+          (curElem) => curElem.price <= price
+        );
+      }
+      return {
+        ...state,
+        filter_products: tempFiltereProduct,
+      };
+    }
+
+    case "CLEAR_FILTERS":
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          text: "",
+          category: "all",
+          company: "all",
+          color: "all",
+          maxPrice: 0,
+          price: state.filters.maxPrice,
+          minPrice: state.filters.minPrice,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
