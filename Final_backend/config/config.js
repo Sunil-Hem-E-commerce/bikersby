c718a3e3fb4b424c453ba85110670d834235dd2f
Hemlal Dulal <hemlaldulal.986@gmail.com>
2023-11-28 20:28:19 +0545

    fix: fixed config.js file

--- Final_backend/config/config.js
+++ Final_backend/config/config.js
@@ -8,10 +8,16 @@ module.exports = {
     database: process.env.DB_DEV_NAME,
     host: process.env.DB_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     migrationStorageTableName: process.env.DB_SEQUELIZE_META,
-    // using render url:
     url: process.env.DB_URL,
   },
+
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
