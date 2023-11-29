d228990e4eeafa6d89031cbb5caf229a7107366d
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-11-29 09:08:45 +0545

    feat: inventory model added

---
+++ Final_backend/models/inventory.js
@@ -0,0 +1,56 @@
+const inventory = (sequelize, DataTypes) =>
+  sequelize.define(
+    "inventory",
+    {
+      inventoryId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "userId",
+        },
+      },
+      itemId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "product_items",
+          key: "itemId",
+        },
+      },
+      isShipping: {
+        type: DataTypes.CHAR,
+        allowNull: false,
+        defaultValue: "y",
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      discounted_price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "inventories",
+      uniqueKeys: {
+        unique_inventory: {
+          fields: ["userId", "itemId"],
+        },
+      },
+    }
+  );
+
+module.exports = inventory;
