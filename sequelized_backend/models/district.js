629890e809972c79dee130cd6ea071d85d1ca7f2
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-01 05:29:54 +0545

    association: address-district

--- sequelized_backend/models/district.js
+++ sequelized_backend/models/district.js
@@ -2,13 +2,11 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class District extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      District.hasMany(models.Address, {
+        foreignKey: "district_id",
+        as: "addresses",
+      });
     }
   }
   District.init(
