69cef40973ed49a3669d1aa65ed67ec9514a0d56
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-01 04:28:20 +0545

    feat: stable version of app to connect to postgres

--- sequelized_backend/app.js
+++ sequelized_backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("./models/");
 
 // For post request
 app.use(express.json());
---
+++ sequelized_backend/models/index.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const process = require("process");
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
+const db = {};
+
+let sequelize;
+if (config.url) {
+  sequelize = new Sequelize(config.url, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(
+      `Postgres connection has been established successfully to database: ${sequelize.config.database} by user: ${sequelize.config.username}`
+    );
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database::", error);
+  });
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
