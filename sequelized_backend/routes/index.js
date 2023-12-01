7d867fb32c55677c5072ac9267739e6bfcea03ac
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-01 08:46:29 +0545

    feat: user post route

--- sequelized_backend/routes/index.js
+++ sequelized_backend/routes/index.js
@@ -2,6 +2,7 @@ const { route } = require("../app");
 
 const router = require("express").Router();
 const userController = require("../controllers").user;
+const loginController = require("../controllers").login;
 
 router.get("/", (req, res, next) => {
   res.send("Bikersby backend homepage");
@@ -10,4 +11,5 @@ router.get("/", (req, res, next) => {
 router.get("/api/users/", userController.list);
 router.post("/api/users/", userController.addUser);
 
+router.post("/api/login/", loginController.loginUser);
 module.exports = router;
