57629721c94cf2f8202be8226b630d6a59a8ec62
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-01 08:56:30 +0545

    feat: product get all list

--- sequelized_backend/controllers/index.js
+++ sequelized_backend/controllers/index.js
@@ -1,4 +1,5 @@
 const user = require("./user");
 const login = require("./login");
+const productType = require("./product_type");
 
-module.exports = { user, login };
+module.exports = { user, login, productType };
--- sequelized_backend/routes/index.js
+++ sequelized_backend/routes/index.js
@@ -3,6 +3,7 @@ const { route } = require("../app");
 const router = require("express").Router();
 const userController = require("../controllers").user;
 const loginController = require("../controllers").login;
+const productTypeController = require("../controllers").productType;
 
 router.get("/", (req, res, next) => {
   res.send("Bikersby backend homepage");
@@ -12,4 +13,6 @@ router.get("/api/users/", userController.list);
 router.post("/api/users/", userController.addUser);
 
 router.post("/api/login/", loginController.loginUser);
+
+router.get("/api/products/", productTypeController.list);
 module.exports = router;
