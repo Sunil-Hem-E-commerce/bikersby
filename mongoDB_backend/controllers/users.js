5601e2edc8ef82810788a63c9a95f939ec4431ca
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-05 22:33:15 +0545

    feat: try-catch block added

--- mongoDB_backend/controllers/users.js
+++ mongoDB_backend/controllers/users.js
@@ -2,27 +2,35 @@ const usersRouter = require("express").Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
-usersRouter.get("/", async (req, res) => {
-  const users = await User.find({}).populate("orders", {});
-  res.json(users);
+usersRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await User.find({}).populate("orders", {});
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
 });
 
 usersRouter.post("/", async (req, res, next) => {
-  const { fullName, email, password } = req.body;
-  if (password.length < 4) {
-    return res.status(400).send("Password cannot be less then 4 characters");
+  try {
+    const { fullName, email, password } = req.body;
+    if (password.length < 4) {
+      return res.status(400).send("Password cannot be less then 4 characters");
+    }
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      fullName,
+      email,
+      passwordHash,
+    });
+
+    const savedUser = await user.save();
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    next(error);
   }
-  const passwordHash = await bcrypt.hash(password, 10);
-
-  const user = new User({
-    fullName,
-    email,
-    passwordHash,
-  });
-
-  const savedUser = await user.save();
-
-  res.status(201).json(savedUser);
 });
 
 module.exports = usersRouter;
