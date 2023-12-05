aae70a785513f4e908579dc4893bc11802eebf11
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-05 22:38:08 +0545

    feat: try-catch block added

--- mongoDB_backend/controllers/login.js
+++ mongoDB_backend/controllers/login.js
@@ -3,35 +3,39 @@ const bcrypt = require("bcrypt");
 const loginRouter = require("express").Router();
 const User = require("../models/user");
 
-loginRouter.post("/", async (request, response) => {
-  const { email, password } = request.body;
+loginRouter.post("/", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  const passwordCorrect =
-    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+    const passwordCorrect =
+      user === null ? false : await bcrypt.compare(password, user.passwordHash);
 
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: "invalid username or password",
+    if (!(user && passwordCorrect)) {
+      return res.status(401).json({
+        error: "invalid username or password",
+      });
+    }
+
+    const userForToken = {
+      email: user.email,
+      id: user._id,
+    };
+
+    const token = jwt.sign(userForToken, process.env.SECRET, {
+      expiresIn: 60 * 5,
     });
+
+    res.status(200).send({
+      token,
+      fullName: user.fullName,
+      email: user.email,
+      user: { id: user._id },
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const userForToken = {
-    email: user.email,
-    id: user._id,
-  };
-
-  const token = jwt.sign(userForToken, process.env.SECRET, {
-    expiresIn: 60 * 5,
-  });
-
-  response.status(200).send({
-    token,
-    fullName: user.fullName,
-    email: user.email,
-    user: { id: user._id },
-  });
 });
 
 module.exports = loginRouter;
