2b4eb5cd770de7ab7470f0eb8ba3ddd084c4bded
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-05 22:15:13 +0545

    feat: login functionality

--- mongoDB_backend/app.js
+++ mongoDB_backend/app.js
@@ -7,7 +7,7 @@ const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const usersRouter = require("./controllers/users");
 // const blogsRouter = require("./controllers/blogs");
-// const loginRouter = require("./controllers/login");
+const loginRouter = require("./controllers/login");
 
 console.log("Connecting to ", config.mongoUrl);
 
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", usersRouter);
-// app.use("/api/login", loginRouter);
+app.use("/api/login", loginRouter);
 // app.use("/api/blogs", blogsRouter);
 
 if (process.env.NODE_ENV === "test") {
--- mongoDB_backend/controllers/login.js
+++ mongoDB_backend/controllers/login.js
@@ -4,9 +4,9 @@ const loginRouter = require("express").Router();
 const User = require("../models/user");
 
 loginRouter.post("/", async (request, response) => {
-  const { username, password } = request.body;
+  const { email, password } = request.body;
 
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ email });
 
   const passwordCorrect =
     user === null ? false : await bcrypt.compare(password, user.passwordHash);
@@ -18,7 +18,7 @@ loginRouter.post("/", async (request, response) => {
   }
 
   const userForToken = {
-    username: user.username,
+    email: user.email,
     id: user._id,
   };
 
@@ -26,14 +26,12 @@ loginRouter.post("/", async (request, response) => {
     expiresIn: 60 * 5,
   });
 
-  response
-    .status(200)
-    .send({
-      token,
-      username: user.username,
-      name: user.name,
-      user: { id: user._id },
-    });
+  response.status(200).send({
+    token,
+    fullName: user.fullName,
+    email: user.email,
+    user: { id: user._id },
+  });
 });
 
 module.exports = loginRouter;
