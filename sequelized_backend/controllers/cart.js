28af969a12068ba1ba1f9d3a787ade27d798963a
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-06 16:55:56 +0545

    delete old sequelized backend

--- sequelized_backend/controllers/cart.js
+++
@@ -1,8 +0,0 @@
-const Cart = require("../models").Cart;
-
-module.exports = {
-  async list(req, res) {
-    const cartList = await Cart.findAll({});
-    res.status(200).send(cartList);
-  },
-};
--- sequelized_backend/controllers/index.js
+++
@@ -1,6 +0,0 @@
-const user = require("./user");
-const login = require("./login");
-const product = require("./product");
-const cart = require("./cart");
-
-module.exports = { user, login, product, cart };
