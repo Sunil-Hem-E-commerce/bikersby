dd817a0f8b7fee4b162eeff42b9f3bdbebcaedb4
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-10 15:31:27 +0545

    feat: product list flattened as required

--- mongoDB_backend/controllers/products.js
+++ mongoDB_backend/controllers/products.js
@@ -4,9 +4,46 @@ module.exports = {
   async list(req, res, next) {
     try {
       const products = await Product.find({})
-        .populate("images")
-        .populate("colors");
-      res.json(products);
+        .populate({
+          path: "images",
+          options: { limit: 1 },
+          select: "url",
+        })
+        .populate({ path: "colors", select: "hex" });
+
+      const flattenedProducts = products.map((product) => {
+        const {
+          _id,
+          name,
+          company,
+          price,
+          discountedPrice,
+          description,
+          category,
+          featured,
+          stock,
+          rating,
+          star,
+          images,
+          colors,
+        } = product;
+        return {
+          id: _id,
+          name,
+          company,
+          price,
+          discountedPrice,
+          description,
+          category,
+          featured,
+          stock,
+          rating,
+          star,
+          image: images[0]?.url,
+          colors: colors.map((color) => color.hex),
+        };
+      });
+      res.json(flattenedProducts);
     } catch (error) {
       next(error);
     }
