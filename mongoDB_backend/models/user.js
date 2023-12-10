84319747bef9699e42d9e40136993a5a27c1bdca
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-10 23:13:17 +0545

    feat: added cart router

--- mongoDB_backend/models/user.js
+++ mongoDB_backend/models/user.js
@@ -8,8 +8,12 @@ const userSchema = new mongoose.Schema({
   address: { type: String },
   orders: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Order",
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+      quantity: { type: Number, min: 0 },
+      status: String,
     },
   ],
 });
