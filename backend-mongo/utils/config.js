9535d325db2a8f672538744334d2744bbe65c07c
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-12-21 08:20:31 +0545

    refactor: del old codes

--- backend-mongo/utils/config.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-const api = process.env.API_URL;
-
-const url = process.env.MONGODB_URI;
-const PORT = process.env.PORT || 3000;
-
-module.exports = { PORT, url, api };
